b2274d092b2a4d3907089cd5e0fdf4e8dd3a9ea1
lipan <alipan484@163.com>
2025-09-12 12:50:49 +0100

    first

---
+++ exercise2.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def caculate_area(base, height):
+    # Use a breakpoint in the code line below to debug your script.
+    area = base * height
+    return area
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    base = int(input("base:"))
+    height = int(input("height:"))
+    caculate_area( base,height)
+    print(caculate_area( base,height))
+
+
